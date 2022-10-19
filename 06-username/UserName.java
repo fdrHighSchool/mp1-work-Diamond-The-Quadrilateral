4968dab5421862cc08e701d31bbb7ffc4fcdd623
AP Java Period 6 <apjavaperiod6@ac1mwk0vmjc9h.78b505.nycboe.org>
2022-10-19 12:28:16 -0400

    Added Extra Layer of Randomization

--- 06-username/UserName.java
+++ 06-username/UserName.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.Random;
+import java.lang.Math;
 public class UserName {
   public static void main(String[] args) {
     // create Scanner object
@@ -15,33 +15,61 @@ public class UserName {
     System.out.print("Are You A Student or a Teacher?: ");
     String whichRole = s.nextLine().toLowerCase();
     
-    
     if(whichRole.equals("teacher")){
-        System.out.print("Your Username Is " + firstLetter(firstName) + (lastName) + favNum+"@schools.nyc.gov");
-        genPassword();
+        System.out.println("Your Username Is " + firstLetter(firstName) + (lastName) + favNum+"@schools.nyc.gov");
+        System.out.println("How Long Would You Like Your Randomly Generated password to be?");
+        int length = s.nextInt();
+        genPassword(length);
     } else if (whichRole.equals("student")){
       System.out.println("Your Username Is " + (firstName) + firstLetter(lastName) + favNum+"@nycstudents.net");
-       genPassword();
+      System.out.println("How Long Would You Like Your Randomly Generated password to be?");
+      int length = s.nextInt();
+       genPassword(length);
     }else{
         System.out.println("I Don't Quite Understand "+ whichRole);
     }
     
     s.close();
 }// end main MO
-public static void genPassword(){
-       Random rand = new Random();
+public static String genPassword(int length){
+        
+        //Placeholder
         String randomPassword = ("");
-      for(int i = 0; i<10; i++){
-     String characterBank = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
- 
+        int max = 0;
+        int min = 0;
+ for(int i = length; i>0; i--){
+      //MO 1
       
-     int grabOne = rand.nextInt(36);
-       String randomLetter = characterBank.substring(grabOne, grabOne+1);
-     randomPassword = (randomPassword + randomLetter);
-    
+      //String characterBank = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+      //int grabOne = rand.nextInt(35);
+      //String randomLetter = characterBank.substring(grabOne, grabOne+1);
+      //randomPassword = (randomPassword + randomLetter);
+      
+      //MO1
+     
+      //ASCI MO
+      int whichList = (int)(Math.random()*4);
+      System.out.println(whichList);
+      if (whichList ==0){ //Capital Letters
+      min = 65;
+      max = 90;
+    }else if(whichList==1){ //Lowercase Letters
+       min = 97;
+       max = 122;
+    }else{
+        min =33;
+        max = 47;
     }
+    
+    int rand = (int)(Math.random()*(max-min)+min); 
+       char addPass = (char)rand;
+       randomPassword = (randomPassword + addPass);
+       
+}
+      //ASCI MO
   System.out.println("Randomly Genned Password: " + randomPassword);
-   
+   return randomPassword;
+
   }// end genPass
 
   /*
@@ -52,6 +80,7 @@ public static void genPassword(){
    */
   public static String firstLetter(String n) {
     return n.substring(0, 1);
+    
   } // end initialize method
 
 } // end class
